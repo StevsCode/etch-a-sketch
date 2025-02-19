05d77ad5afe3b158abac1b2fbc0f36e11e9dbeee
StevsCode <stevensavarese1@gmail.com>
2025-02-19 17:52:29 +0100

    In js, remove the = sign in the for cycle. In css, align container horizontally and set its space and insert every grid-item inside the container

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const container = document.querySelector(".container")
 
 // Declare newDiv and append it to the container
-for (let i = 0; i <= 256; i++) {
+for (let i = 0; i < 256; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("grid-item"); // Add class to newDiv
     container.appendChild(newDiv);
