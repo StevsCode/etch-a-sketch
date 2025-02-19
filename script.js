60554ce77af2666d55dd591e9d1ab2c7791cfb6a
StevsCode <stevensavarese1@gmail.com>
2025-02-19 17:15:36 +0100

    Add class to newDiv in js

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ const container = document.querySelector(".container")
 // Declare newDiv and append it to the container
 for (let i = 0; i <= 256; i++) {
     const newDiv = document.createElement("div");
+    newDiv.classList.add("grid-item"); // Add class to newDiv
     container.appendChild(newDiv);
 }
+
+// 
